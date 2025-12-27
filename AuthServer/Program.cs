c83b3870862d72d79f614214aa4610962220beeb
Arpit-0001 <mysteriousgamer0001@gmail.com>
2025-12-27 17:53:22 +0530

    Update Program.cs

--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -98,7 +98,7 @@ static JsonObject EncryptApiObject(JsonObject obj, string session, string hwid)
     var encrypted = new JsonObject();
     foreach (var kv in obj)
     {
-        encrypted[kv.Key] = Hmac(kv.Value!.ToString() + key);
+        encrypted[kv.Key] = Hmac($"{kv.Key}:{kv.Value}:{key}");
     }
     return encrypted;
 }
